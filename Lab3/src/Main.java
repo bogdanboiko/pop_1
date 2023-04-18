4c2d4bef9209ab0b5190aff14dd0a0d7c1f7681d
bogdanboiko <bogdanboiko19996@gmail.com>
2023-04-18 21:58:25 +0300

    add lab 3 ada

--- Lab3/src/Main.java
+++ Lab3/src/Main.java
@@ -1,20 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        int storageSize = 8;
-        int itemNumbers = 30;
+        int storageSize = 3;
+        int itemNumbers = 10;
 
-        int producerQuantity = 6;
-        int consumerQuantity = 2;
+        int producerQuantity = 7;
+        int consumerQuantity = 5;
 
         Manager manager = new Manager(storageSize);
-
 
         int producerStep = itemNumbers / producerQuantity;
         int producerIdCounter = 1;
 
-        for (int i = 0; i < itemNumbers; i += producerStep) {
-            if (i + producerStep > itemNumbers) {
-                new Producer(producerIdCounter, itemNumbers - i, manager);
+
+        System.out.println(producerStep);
+
+        for (int i = 0; i < producerQuantity; i++) {
+            if (i == producerQuantity - 1 && itemNumbers % producerQuantity > 0) {
+                new Producer(producerIdCounter, itemNumbers - i * producerStep, manager);
             } else {
                 new Producer(producerIdCounter, producerStep, manager);
             }
@@ -24,9 +26,11 @@ public class Main {
         int consumerStep = itemNumbers / consumerQuantity;
         int consumerIdCounter = 1;
 
-        for (int i = 0; i < itemNumbers; i += consumerStep) {
-            if (i + consumerStep > itemNumbers) {
-                new Consumer(consumerIdCounter, itemNumbers - i, manager);
+        System.out.println(consumerStep);
+
+        for (int i = 0; i < consumerQuantity; i++) {
+            if (i == consumerQuantity - 1 && itemNumbers % consumerQuantity > 0) {
+                new Consumer(consumerIdCounter, itemNumbers - i * consumerStep, manager);
             } else {
                 new Consumer(consumerIdCounter, consumerStep, manager);
             }
